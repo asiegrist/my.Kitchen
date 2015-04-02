5c914f1375ed6982b898bbbddd99a4da96987257
asiegrist <asiegrist@hotmail.fr>
2015-04-02 10:04:25 +0200

    Registration Form with some validation added

--- client/js/app.js
+++ client/js/app.js
@@ -2,21 +2,31 @@
 
 var myKitchen = angular.module('myKitchen', [
   'ngRoute',
+  'ngPassword',
   'recipeControllers',
+  'userControllers'
 ]);
 
 myKitchen.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider
       .when('/recipe', {
-        templateUrl: 'templates/recipe-list.html',
+        templateUrl: 'html/templates/recipe-list.html',
         controller: 'RecipeListCtrl',
       })
       .when('/recipe/:recipeId', {
-        templateUrl: 'templates/recipe-detail.html',
+        templateUrl: 'html/templates/recipe-detail.html',
         controller: 'RecipeDetailCtrl',
       })
+	  .when('/user/registration', {
+		  templateUrl: 'html/templates/user-registration.html',
+		  controller: 'UserRegistrationCtrl',
+	  })
+	  .when('/user/connection', {
+		  templateUrl: 'html/templates/user-connection.html',
+		  controller: 'UserConnectionCtrl',
+	  })
       .otherwise({
-        redirectTo: '/recipe',
+        redirectTo: '/user/connection',
       });
   }]);
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var recipeControllers = angular.module('recipeControllers', []);
+var userControllers = angular.module('userControllers', []);
 
 recipeControllers.controller('RecipeListCtrl', ['$scope', '$http',
   function($scope, $http) {
@@ -18,5 +19,30 @@ recipeControllers.controller('RecipeDetailCtrl', [
     $http.get('api/recipe/' + $routeParams.recipeId).success(function(data) {
       $scope.recipe = data;
     });
-  }]
-);
+  }
+]);
+
+userControllers.controller('UserRegistrationCtrl', ['$scope',
+	function($scope) {
+		this.createUser = function() {
+			if($scope.registrationForm.$valid) {
+				if(!this.gender){
+					this.gender = "female";
+				}
+				if(!this.newsletter) {
+					this.newsletter=false;
+				}
+				alert("Merci de vous être inscrit");
+			}
+			else {
+				alert("Veuillez remplir tous les champs obligatoire.");
+			}
+		};
+	}
+]);
+
+userControllers.controller('UserConnectionCtrl', ['$scope',
+	function($scope) {
+		
+	}
+]);
